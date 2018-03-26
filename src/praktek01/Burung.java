5643e4df48763f1687f3b07f7fab4f0da185a977
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-03-26 17:10:52 +0700

    membuat atribut dan behavior

--- src/praktek01/Burung.java
+++ src/praktek01/Burung.java
@@ -10,5 +10,19 @@ package praktek01;
  * @author proWindows8.1
  */
 public class Burung {
-    
+    String nama;
+    String warna_burung;
+    String makanan;
+    String panjang_burung;
+    String bentuk_ekor;
+    String tinggi_burung;
+
+void cara_terbang(){
+    System.out.println("nama : "+nama);
+    System.out.println("warna_burung : "+warna_burung);
+    System.out.println("makanan : "+makanan);
+    System.out.println("panjang_burung : "+panjang_burung);
+   System.out.println("bentuk_ekor : "+bentuk_ekor);
+    System.out.println("tinggi_burung : "+tinggi_burung);
+            }
 }
