3bf48f2af67ff4b1a9966252119245102693b204
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-03-26 17:28:18 +0700

    mengisi nilai atribut

--- src/praktek01/BurungAksi.java
+++ src/praktek01/BurungAksi.java
@@ -14,5 +14,26 @@ public class BurungAksi {
         Burung Elang = new Burung ();
         Burung Merak = new Burung ();
         Burung Gagak = new Burung ();
+        
+        Elang.nama="Elang";
+        Elang.warna_burung="Coklat Emas";
+        Elang.makanan="Daging/tikus,ikan";
+        Elang.panjang_burung="70-80cm";
+        Elang.bentuk_ekor="Panjang kerucut";
+        Elang.tinggi_burung="90cm";
+        
+        Gagak.nama="Gagak";
+        Gagak.warna_burung="Hitam pekat";
+        Gagak.makanan="Sejenis bangkai daging";
+        Gagak.panjang_burung="56-78cm";
+        Gagak.bentuk_ekor="Lebar mengembang";
+        Gagak.tinggi_burung="50cm";
+        
+        Merak.nama="Merak";
+        Merak.warna_burung="Hijau kebiru-biruan";
+        Merak.makanan="Biji-bijian/serangga";
+        Merak.panjang_burung="100-120cm";
+        Merak.bentuk_ekor="Mengembang sangat lebar";
+        Merak.tinggi_burung="70cm";
     }
 }
